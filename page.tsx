ac672c73c099d426cba9806e969c5c6e4e3c8f2b
Muhammad Usman Asif <164504890+Usman-Philco123@users.noreply.github.com>
2024-10-16 20:55:52 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,11 @@
+
+
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div className='bg-red-700 text-white '>
+      <h1>This is my Home Page</h1>
+    </div>
+  )
+}
